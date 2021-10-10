0108c2b0a0a61fd27bb58e4129768391cdd2229c
akkradet <akkradet7@gmail.com>
2021-10-10 20:01:41 +0700

    Initial commit

---
+++ addons/auto_install_oca_thailand/__manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Auto Install OCA thailand",
+    "summary": "Used to install OCA thailand modules",
+    "version": "1.0",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "author": "Akkradet",
+    "installable": True,
+    "depends": [
+        # l10n-thailand
+        "l10n_th_amount_to_text",
+        "l10n_th_base_location",
+		    "l10n_th_fonts",
+        "l10n_th_partner",
+        "l10n_th_tax_invoice",
+        "l10n_th_tax_report",
+        "l10n_th_withholding_tax",
+        "l10n_th_withholding_tax_cert",
+        "l10n_th_withholding_tax_cert_form",
+
+    ],
+    "data": [],
+}
